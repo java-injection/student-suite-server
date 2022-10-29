57f696d17a67c1c55fc49814585cc30e7d514926
Luca Coraci <luca.coraci@gmail.com>
2022-10-29 11:26:23 +0200

    first commit

---
+++ src/test/java/it/ji/server/StudentSuiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ji.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSuiteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
