4bb77b56105116e1bbb30a78ece5fea23e849f3c
Luca Coraci <luca.coraci@gmail.com>
2022-10-30 11:17:21 +0100

    student repository

--- src/main/java/it/ji/server/services/StudentService.java
+++ src/main/java/it/ji/server/services/StudentService.java
@@ -1,6 +1,8 @@
 package it.ji.server.services;
 
 import it.ji.server.models.Student;
+import it.ji.server.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,11 +11,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping
     public List<Student> getAllStudents() {
-        return List.of(
-                new Student("Luca", "Coraci", 13),
-                new Student("Mario", "Trapano", 15)
-                );
+        return this.studentRepository.findAll();
     }
 }
